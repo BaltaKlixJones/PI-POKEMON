6598fad4ec876e10a9855a82e9bc102ddb366314
BaltaKlixJones <balta98_@hotmail.com>
2023-01-18 09:48:33 -0300

    routes

--- api/src/routes/pokemonsRoute.js
+++ api/src/routes/pokemonsRoute.js
@@ -18,22 +18,22 @@ router.get("/getAll", async (req, res) => {
     const url2a = await axios.get(url2);
     const response = url2a.data.results;
     //console.log("response:", response)
-    //console.log("DATAAAA: ",data)
+    console.log("DATAAAA: ", url2a);
 
+    let i = 20;
 
-    let i = 0
-    const { url } = response[1];
-    
+    console.log("RESPONSE", response[i]);
 
-    const { data } = await axios.get(url);
     //console.log("DATAAAAAA",data)
+    const { url } = response[1];
+    const { data } = await axios.get(url);
 
     //console.log(data.next);
     const pokemon = response.map((poke) => {
       // arreglo con los personajes de la API
-      i++
+      i++;
       const obj = {
-        id: i,
+        id: data.id++,
         name: poke.name,
         hp: data.stats[0].base_stat,
         attack: data.stats[1].base_stat,
@@ -44,19 +44,18 @@ router.get("/getAll", async (req, res) => {
         img: data.sprites.other.dream_world.front_default,
       };
       return obj;
-    })
-     
-    console.log("DATAAAA: ",data.id)
+    });
+
+    console.log("DATAAAA: ", response.id);
     // console.log("POKEMONS INFO ",pokemonsInfo)
 
     const bd = await Pokemon.findAll({ include: [{ model: Type }] }); // arreglo con los personajes de la bd
     const suma = [...pokemon, ...bd];
     res.status(200).json(suma);
   } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+    res.status(400).json({ error: error.message });
+  }
 });
-
 
 // GET /pokemons/{idPokemon}:
 // Obtener el detalle de un pokemon en particular
@@ -75,6 +74,49 @@ router.get("/getAll", async (req, res) => {
 // Recibe los datos recolectados desde el formulario controlado de la ruta de creación de pokemons por body
 // Crea un pokemon en la base de datos relacionado con sus tipos.
 
-// router.post("/create", (req, res) => {});
+router.post("/create", async (req, res) => {
+  const { name, hp, attack, defense, speed, img, height, weight, types } = req.body;
+
+  if (
+    !name ||
+    !hp ||
+    !attack ||
+    !defense ||
+    !speed ||
+    !height ||
+    !weight
+  )
+    res.status(400).json({ msg: "Faltan datos" }); // validar los datos
+  try {
+    const obj = { name, hp, attack, defense, speed, height, weight, types }
+    const nvoPokemon = await Pokemon.create(obj)
+
+    // console.log(nvoPokemon.__proto__);
+    nvoPokemon.addType(types) // relaciona el pokemon nuevo con el tipo 
+
+    res.status(200).send(nvoPokemon)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+});
+
+// get types 
+
+router.get("/types", async (req, res) => {
+    try {
+        await axios.get("https://pokeapi.co/api/v2/type").then(response => {
+        let aux = response.data.results.map(ty => {
+            const obj = {
+                id: ty.id, 
+                type: ty.name
+            }
+            return obj 
+        }) 
+        res.status(200).send(aux)
+    })
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router;
