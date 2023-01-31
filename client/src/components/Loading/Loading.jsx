f7ee550485c08056ea844eaa98bdd05a0176d95c
BaltaKlixJones <balta98_@hotmail.com>
2023-01-31 13:28:12 -0300

    Terminado

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -9,13 +9,7 @@ export default function Loading () {
         <div className="loading">
             <img  src="https://media.baamboozle.com/uploads/images/125978/1629738053_29014_gif-url.gif" alt="" />
             <p style={{color:"white"}}>Espera un segundo ...</p>
-            <Link to="/home">
-        <div className="">
-          <button className="reload-button" onClick={"location.reload()"}>
-            <i className="fas fa-sync-alt">Volver</i>
-          </button>
-        </div>
-      </Link>
+            
         </div>
         </div>
     )
