0f767be58abd2e32c9f14a3117a73a656222871f
BaltaKlixJones <balta98_@hotmail.com>
2023-01-31 12:58:11 -0300

    Terminado

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Error.css';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
     <div className="error-container">
-      <h1 className="error-title">Error 404</h1>
+      <h1 className="error-title">Error! </h1>
       <p className="error-message">No se encontraron resultados!</p>
+      <Link to="/home">
+      <button className="reload-button" onClick={"location.reload()"}>
+            <i className="fas fa-sync-alt">Volver</i>
+          </button>
+          </Link>
     </div>
   );
 }
